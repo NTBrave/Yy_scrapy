741e4434001d19421febe907cccb0498de87206b
NTBrave <1398811892@qq.com>
2019-07-03 00:00:18 +0800

    yy
    第一次上传

---
+++ Yy/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json, codecs
+import pymongo
+
+class YyPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('Yyanchor.csv', 'wb+', encoding='utf-8')  # 创建以utf-8编码的csv文件
+        client = pymongo.MongoClient('localhost', 27017)  # 创建mongodb连接
+        db = client['Yy99']  # 创建mongodb数据库huya
+        self.collection = db['Yyanchor']  # 创建数据库huya中collection
+
+    def process_item(self, item, spider):
+        item = dict(item)  # 将抓到的item转为dict格式
+        line = json.dumps(item) + '\n'  # 定义line字段将抓到的item转为jump格式，加上空格换行
+        self.file.write(line)  # 将line写进csv中输出 修改了decode（）
+        self.collection.insert(item)  # 将item写进mongodb中
+        pass
---
+++ 跑爬虫_先改里面路径.bat
@@ -0,0 +1,5 @@
+@echo off 
+F:
+cd  F:\pyTest\Yy
+scrapy crawl yyspider
+pause
